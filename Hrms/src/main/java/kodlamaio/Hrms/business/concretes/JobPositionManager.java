53ccf479850587de5beedc9531f72b0b863252f0
HasanCihad <45674149+HasanCihad@users.noreply.github.com>
2021-05-12 18:02:22 +0300

    Add files via upload

---
+++ Hrms/src/main/java/kodlamaio/Hrms/business/concretes/JobPositionManager.java
@@ -0,0 +1,32 @@
+package kodlamaio.Hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.Hrms.business.abstracts.JobPositionService;
+import kodlamaio.Hrms.dataAccess.abstracts.JobPositionDao;
+import kodlamaio.Hrms.entities.concretes.JobPosition;
+
+@Service
+public class JobPositionManager implements JobPositionService {
+
+	private JobPositionDao jobPositionDao;
+	
+	
+	@Autowired
+	public JobPositionManager(JobPositionDao jobPositionDao) {
+		super();
+		this.jobPositionDao = jobPositionDao;
+	}
+
+
+
+	@Override
+	public List<JobPosition> getAll() {
+		// TODO Auto-generated method stub
+		return this.jobPositionDao.findAll();
+	}
+
+}
